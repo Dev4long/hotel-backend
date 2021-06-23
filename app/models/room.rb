8c560ee6d3096c183afa0e8a281d818d793f514a
deniskadrich <dev4long@live.com>
2021-06-23 18:32:58 -0400

    ddd

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,11 @@
 class Room < ApplicationRecord
-    has_many :stays
+    has_many :stays, dependent: :destroy
     has_many :users, through: :stays
 
     belongs_to :all_hotel
+
+   
     
 end
+
+# , dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
   namespace :v1 do 
 
-    resources :rooms, only: [:index]
+    resources :rooms, only: [:index, :destroy]
     resources :stays, only: [:index, :create]
     resources :users, only: [:index]
     resources :all_hotels, only: [:index]
